feb8db9fdab949e2f0d6ef72451a84e706fb6817
yfchen <yinfan.chen@njust.edu.cn>
2023-08-22 11:50:08 +0800

    第一章完结

---
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -0,0 +1,76 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.AlphaDao;
+import com.nowcoder.community.service.AlphaService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@RunWith(SpringRunner.class)  // @RunWith注解指定了测试运行器为SpringRunner，可以使用Spring的依赖注入功能
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)  // @ContextConfiguration注解指定了加载上下文的配置类为CommunityApplication，这意味着在测试中，将会加载CommunityApplication类中定义的Bean
+public class CommunityApplicationTests implements ApplicationContextAware {
+
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	@Test
+	public void testApplicationContext() {
+		System.out.println(applicationContext);
+
+		AlphaDao alphaDao = applicationContext.getBean(AlphaDao.class);
+		System.out.println(alphaDao.select());
+
+		alphaDao = applicationContext.getBean("alphaHibernate", AlphaDao.class);
+		System.out.println(alphaDao.select());
+	}
+
+	@Test
+	public void testBeanManagement() {
+		AlphaService alphaService = applicationContext.getBean(AlphaService.class);
+		System.out.println(alphaService);
+
+		alphaService = applicationContext.getBean(AlphaService.class);
+		System.out.println(alphaService);
+	}
+
+	@Test
+	public void testBeanConfig() {
+		SimpleDateFormat simpleDateFormat =
+				applicationContext.getBean(SimpleDateFormat.class);
+		System.out.println(simpleDateFormat.format(new Date()));
+	}
+
+	@Autowired
+	@Qualifier("alphaHibernate")
+	private AlphaDao alphaDao;
+
+	@Autowired
+	private AlphaService alphaService;
+
+	@Autowired
+	private SimpleDateFormat simpleDateFormat;
+
+	@Test
+	public void testDI() {
+		System.out.println(alphaDao);
+		System.out.println(alphaService);
+		System.out.println(simpleDateFormat);
+	}
+
+}
